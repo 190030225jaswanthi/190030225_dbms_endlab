bd30a451cc5c542f4962d4e6e0ba63bdba1afcb6
190030225jaswanthi <74001979+190030225jaswanthi@users.noreply.github.com>
2021-08-25 09:34:42 +0530

    Add files via upload

---
+++ 190030225_prac_dbms.sql
@@ -0,0 +1,5 @@
+select c.cust_id,c.cust_name from customers c inner join branch b on b.c_id=c.cust_id;
+select cust_name,city,ph_no((year,dob,getdate() as Age cutomers innnerjoin(select min[dob]y) a on a.y=dob);
+select veh_name from vehicle;
+select distinct city from curtsomer;
+select sum(amount_per_seat) total amount from contract_permisiion;
